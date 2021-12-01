c16922e4ab9fbe1589d498af53f2b5d85e360779
andrewVP41 <61993180+andrewVP41@users.noreply.github.com>
2021-12-01 05:26:37 -0500

    Configs

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ config.production = {
   name: "production",
   httpPort: 8080,
   httpsPort: 443,
-  database: process.env.DATABASE,
-  secret: process.env.SECRET,
+  database: "mongodb://user:film-1234@cluster0-shard-00-00.h81bv.mongodb.net:27017,cluster0-shard-00-01.h81bv.mongodb.net:27017,cluster0-shard-00-02.h81bv.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-143sjf-shard-0&authSource=admin&retryWrites=true&w=majority",
+  secret: "film-1234",
 };
 
 // Change before deploying
